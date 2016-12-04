6b943c06f54ee1fe7f77aef84c9c6e4a8f9d801a
Mikael Muszynski <linduxed@gmail.com>
2016-12-04 04:00:19 +0100

    Add executable with usage message and tests
    This patch adds the following:
    
    * The rough structure of the executable
    * A helper for testing executables
    * Ability to display usage for "usage", "-h" and "--help"
    * Tests for displaying usage

---
+++ spec/print_usage_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe 'Printing usage' do
+  context 'user requests usage with "todo.sh mit usage"' do
+    specify 'usage message is printed' do
+      executable = Executable.run('usage')
+
+      expect(executable.error).to be_empty, "Error:\n#{executable.error}"
+      expect(executable.exit_code).to eq(0)
+      usage_message_shown?(executable)
+    end
+  end
+
+  context 'user requests usage with "todo.sh mit --help"' do
+    specify 'usage message is printed' do
+      executable = Executable.run('--help')
+
+      expect(executable.error).to be_empty, "Error:\n#{executable.error}"
+      expect(executable.exit_code).to eq(0)
+      usage_message_shown?(executable)
+    end
+  end
+
+  context 'user requests usage with "todo.sh mit -h"' do
+    specify 'usage message is printed' do
+      executable = Executable.run('-h')
+
+      expect(executable.error).to be_empty, "Error:\n#{executable.error}"
+      expect(executable.exit_code).to eq(0)
+      usage_message_shown?(executable)
+    end
+  end
+
+  def usage_message_shown?(executable)
+    expect(executable.lines).not_to be_empty
+    [
+      /MIT/,
+      /-h.+--help.+help/,
+    ].each do |regex|
+      expect(executable.lines).to include(regex)
+    end
+  end
+end
