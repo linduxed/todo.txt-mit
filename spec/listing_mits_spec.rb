a56a3796bc2687b3739cf75e8dd747b599b65e1c
Mikael Muszynski <linduxed@gmail.com>
2016-12-17 17:43:00 +0100

    Add formatting for headers
    While grouping the MITs by day has been working, this patch makes the
    headers above each group a bit more informative than just having a date
    there.
    
    This patch also introduces a deviation from the original add-on: where
    the original would have headers like "saturday, december 17" when if the
    MIT was more than seven days into the future, this patch makes the
    headers include information such as "next week" and "4 weeks from now".

--- spec/listing_mits_spec.rb
+++ spec/listing_mits_spec.rb
@@ -71,11 +71,149 @@ describe 'Listing MITs' do
       end
 
       context 'with two MITs due today' do
-        specify 'MITs are listed with "Today"-header before them'
+        specify 'MITs are listed with "Today"-header before them' do
+          two_mits_today = <<-EOF
+            2016-11-26 {2016.12.01} Make phone call @personal
+            2016-11-26 {2016.12.01} Play guitar @personal
+          EOF
+
+          with_fixed_time_and_todo_file('2016-12-01', two_mits_today) do |env_extension|
+            executable = Executable.run(env_extension: env_extension)
+
+            expect(executable.error).to be_empty, "Error:\n#{executable.error}"
+            expect(executable.exit_code).to eq(0)
+            expect(executable.lines).to include(/\Atoday/i)
+            today_header_line = executable.lines.index do |line|
+              line.match(/\Atoday/i)
+            end
+            lines_following_header =
+              executable.lines[today_header_line + 1..-1]
+            expect(lines_following_header).to include(/Make phone call/)
+            expect(lines_following_header).to include(/Play guitar/)
+          end
+        end
       end
 
       context 'with two MITs due tomorrow' do
-        specify 'MITs are listed with "Tomorrow"-header before them'
+        specify 'MITs are listed with "Tomorrow"-header before them' do
+          two_mits_tomorrow = <<-EOF
+            2016-11-26 {2016.12.02} Make phone call @personal
+            2016-11-26 {2016.12.02} Play guitar @personal
+          EOF
+
+          with_fixed_time_and_todo_file('2016-12-01', two_mits_tomorrow) do |env_extension|
+            executable = Executable.run(env_extension: env_extension)
+
+            expect(executable.error).to be_empty, "Error:\n#{executable.error}"
+            expect(executable.exit_code).to eq(0)
+            expect(executable.lines).to include(/\Atomorrow/i)
+            tomorrow_header_line = executable.lines.index do |line|
+              line.match(/\Atomorrow/i)
+            end
+            lines_following_header =
+              executable.lines[tomorrow_header_line + 1..-1]
+            expect(lines_following_header).to include(/Make phone call/)
+            expect(lines_following_header).to include(/Play guitar/)
+          end
+        end
+      end
+
+      context 'with three MITs due in 3-7 days' do
+        specify 'MITs are listed with weekday headers before them' do
+          two_mits_tomorrow = <<-EOF
+            2016-11-26 {2016.12.05} Make phone call @personal
+            2016-11-26 {2016.12.06} Respond to email @work
+            2016-11-26 {2016.12.08} Send email about delivery @work
+          EOF
+
+          with_fixed_time_and_todo_file('2016-12-01', two_mits_tomorrow) do |env_extension|
+            executable = Executable.run(env_extension: env_extension)
+
+            expect(executable.error).to be_empty, "Error:\n#{executable.error}"
+            expect(executable.exit_code).to eq(0)
+            expect(executable.lines).to include(/\Amonday/i)
+            expect(executable.lines).to include(/\Atuesday/i)
+            expect(executable.lines).to include(/\Athursday/i)
+
+            monday_header_line = executable.lines.index do |line|
+              line.match(/\Amonday/i)
+            end
+            line_following_header = executable.lines[monday_header_line + 1]
+            expect(line_following_header).to match(/Make phone call/)
+
+            tuesday_header_line = executable.lines.index do |line|
+              line.match(/\Atuesday/i)
+            end
+            line_following_header = executable.lines[tuesday_header_line + 1]
+            expect(line_following_header).to match(/Respond to email/)
+
+            thursday_header_line = executable.lines.index do |line|
+              line.match(/\Athursday/i)
+            end
+            line_following_header = executable.lines[thursday_header_line + 1]
+            expect(line_following_header).to match(/Send email about delivery/)
+          end
+        end
+      end
+
+      context 'with a MIT due in >7 days during next week' do
+        specify 'MIT is listed with "next week"-header before it' do
+          one_mit_next_week = <<-EOF
+            2016-11-26 {2016.12.09} Make phone call @personal
+          EOF
+
+          with_fixed_time_and_todo_file('2016-12-01', one_mit_next_week) do |env_extension|
+            executable = Executable.run(env_extension: env_extension)
+
+            expect(executable.error).to be_empty, "Error:\n#{executable.error}"
+            expect(executable.exit_code).to eq(0)
+            expect(executable.lines).to include(/\Afriday.+next week/i)
+
+            next_week_header_line = executable.lines.index do |line|
+              line.match(/\Afriday.+next week/i)
+            end
+            line_following_header = executable.lines[next_week_header_line + 1]
+            expect(line_following_header).to match(/Make phone call/)
+          end
+        end
+      end
+
+      context 'with three MITs due in >7 days, not next week' do
+        specify 'MITs are listed with headers with week count before them' do
+          three_future_mits = <<-EOF
+            2016-11-26 {2016.12.15} Make phone call @personal
+            2016-11-26 {2017.01.01} Update LICENSE file @personal
+            2016-11-26 {2017.01.09} Send email about delivery @work
+          EOF
+
+          with_fixed_time_and_todo_file('2016-12-01', three_future_mits) do |env_extension|
+            executable = Executable.run(env_extension: env_extension)
+
+            expect(executable.error).to be_empty, "Error:\n#{executable.error}"
+            expect(executable.exit_code).to eq(0)
+            expect(executable.lines).to include(/\Athursday.+2 weeks/i)
+            expect(executable.lines).to include(/\Asunday.+4 weeks/i)
+            expect(executable.lines).to include(/\Amonday.+6 weeks/i)
+
+            two_week_header_line = executable.lines.index do |line|
+              line.match(/\Athursday.+2 weeks/i)
+            end
+            line_following_header = executable.lines[two_week_header_line + 1]
+            expect(line_following_header).to match(/Make phone call/)
+
+            four_week_header_line = executable.lines.index do |line|
+              line.match(/\Asunday.+4 weeks/i)
+            end
+            line_following_header = executable.lines[four_week_header_line + 1]
+            expect(line_following_header).to match(/Update LICENSE file/)
+
+            six_week_header_line = executable.lines.index do |line|
+              line.match(/\Amonday.+6 weeks/i)
+            end
+            line_following_header = executable.lines[six_week_header_line + 1]
+            expect(line_following_header).to match(/Send email about delivery/)
+          end
+        end
       end
     end
   end
