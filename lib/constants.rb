93883b3bbd1b3dee425b7ce9e1d86bd327772eb8
Mikael Muszynski <linduxed@gmail.com>
2017-01-25 02:03:30 +0100

    todo.txt-mit 0.5.0
    Implement context filtering, i.e. the `todo.sh mit @foobar` and
    `todo.sh mit not @foobar` commands.
    
    Minor style fixes and refactorings were also made.

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,5 +1,5 @@
 module Constants
   TODAY = ENV.key?('FIXED_DATE') ? Date.parse(ENV['FIXED_DATE']) : Date.today
   MIT_DATE_REGEX = /\{(\d{4}\.\d{2}\.\d{2})\}/
-  VERSION = 'v0.4.1'.freeze
+  VERSION = 'v0.5.0'.freeze
 end
